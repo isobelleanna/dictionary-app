4b696e62537e2e763acec4410b3566bd008894b1
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-03-10 16:46:44 +0000

    add API documentation Link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -9,7 +9,7 @@ export default function Dictionary() {
     }
     function search(event) {
         event.preventDefault();
-
+        //Documentation- https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
     }
